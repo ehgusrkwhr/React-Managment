06f4d574ad37c4ec58fefb8f8f9f8cb6a510f7a4
dodo <ehgusrkwhr@naver.com>
2019-06-03 00:29:52 +0900

    Create React project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo"/>
+        <h2>관리프로그램</h2>
+      </div>
+   );
+  
+  
+}
+
+export default App;
