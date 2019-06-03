b18b5e1d9af7832acb997563ee83b1f31d754892
dodo <ehgusrkwhr@naver.com>
2019-06-04 00:42:07 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App(){
 
+
+const customer = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '도도',
+  'birthday' : '920128',
+  'gender' : '남자',
+  'job' : '직장인'
+ },
+ {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '960128',
+  'gender' : '남자',
+  'job' : '프로그래머'
+ },
+ {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '김다정',
+  'birthday' : '990128',
+  'gender' : '여자',
+  'job' : '경리'
+ }
+]
+
+function App(){
+  
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>관리프로그램</h2>
+      <div>
+        {
+          customer.map(c => {
+            return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.name}
+            gender={c.gender}
+            job={c.job}
+            />
+              );
+          })
+        }
       </div>
    );
   
   
 }
+// class App extends Component(){
+//   reunder(){
+//     return (
+//       <Customer/>
+//    );
+//   }
+
+// }
 
 export default App;
